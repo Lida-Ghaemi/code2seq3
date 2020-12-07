64b0dae4f9d6f70bf81a37257108eaea53d92a6f
Lida-Ghaemi <74499752+Lida-Ghaemi@users.noreply.github.com>
2020-12-07 12:32:11 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
         config.NUM_EPOCHS = 3000
         config.SAVE_EVERY_EPOCHS = 1
         config.PATIENCE = 10
-        config.BATCH_SIZE = 512
+        config.BATCH_SIZE = 256
         config.TEST_BATCH_SIZE = 256
         config.READER_NUM_PARALLEL_BATCHES = 1
         config.SHUFFLE_BUFFER_SIZE = 10000
@@ -19,7 +19,7 @@ class Config:
         config.NUM_DECODER_LAYERS = 1
         config.MAX_PATH_LENGTH = 8 + 1
         config.MAX_NAME_PARTS = 5
-        config.MAX_TARGET_PARTS = 6
+        config.MAX_TARGET_PARTS = 7
         config.EMBEDDINGS_DROPOUT_KEEP_PROB = 0.75
         config.RNN_DROPOUT_KEEP_PROB = 0.5
         config.BIRNN = True
