40454acd1460afbbf9ef92c42ca7198bf7d98d07
Lida-Ghaemi <74499752+Lida-Ghaemi@users.noreply.github.com>
2020-12-14 13:57:34 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
     @staticmethod
     def get_default_config(args):
         config = Config(args)
-        config.NUM_EPOCHS = 8
+        config.NUM_EPOCHS = 11
         config.SAVE_EVERY_EPOCHS = 1
         config.PATIENCE = 10
         config.BATCH_SIZE = 256
@@ -72,7 +72,7 @@
     @staticmethod
     def get_debug_config(args):
         config = Config(args)
-        config.NUM_EPOCHS = 3000
+        config.NUM_EPOCHS = 11
         config.SAVE_EVERY_EPOCHS = 100
         config.PATIENCE = 200
         config.BATCH_SIZE = 7
